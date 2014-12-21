3ef47f00b0ab655a167e4ba945efdca6a1ec22cd
Nick Cox <nickcox1008@gmail.com>
2014-12-21 14:46:42 -0700

    the beginnings of a simple indexedDB database interface

---
+++ db.js
@@ -0,0 +1,63 @@
+function IndexedDB(store) {
+	this.db = null;
+	this.store = store;
+}
+
+IndexedDB.prototype.onError = function(e) {
+	console.log(e);
+};
+
+IndexedDB.prototype.open = function(version) {
+	var request = indexedDB.open(this.store, version);
+	request.onupgradeneeded = function(e) {
+		var db = e.target.result;
+		e.target.transaction.onerror = this.onError;
+		
+		if (db.objectStoreNames.contains(this.store)) {
+			db.deleteObjectStore(this.store);
+		}
+		
+		var store = db.createObjectStore(this.store, {
+			keyPath: 'id'
+		});
+	};
+	
+	request.onsuccess = function(e) { this.db = e.target.result; };
+	request.onerror = this.onError;
+};
+
+IndexedDB.prototype.insert = function(data, callback) {
+	var trans = this.db.transaction([this.store], 'readwrite');
+	var request = trans.objectStore(this.store).put($.extend({
+		'id': new Date().getTime()
+	}, data));
+	
+	trans.oncomplete = function(e) { callback(e.target.result); };
+	request.onerror = this.onError;
+};
+
+IndexedDB.prototype.getAll = function(callback) {
+	var trans = this.db.transaction([this.store], 'read');
+	var range = IDBKeyRange.lowerBound(0);
+	var request = trans.objectStore(this.store).openCursor(range);
+	
+	request.onsuccess = function(e) {
+		var result = e.target.result;
+		if (!!result === false) {
+			return;
+		}
+		callback(result);
+		result.continue();
+	}
+	request.onerror = this.error;
+};
+
+IndexedDB.prototype.delete = function(id, callback) {
+	var trans = this.db.transaction([this.store], 'readwrite');
+	var request = trans.objectStore(this.store).delete(id);
+	
+	trans.oncomplete = function(e) { callback(e.target.result); };
+	request.onerror = this.onError;
+};
+
+
