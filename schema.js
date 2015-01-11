e403643ce09c6387ff5d6fcb6a8f3b3965b457af
Nick Cox <nickcox1008@gmail.com>
2015-01-11 10:32:14 -0700

    update db schema

--- schema.js
+++ schema.js
@@ -1,12 +1,17 @@
 transactions: [
 	{
-		id: new Date.getTime(),
+		id: new Date.getTime(), // createdAt
 		date: Date,
 		name: 'Car Payment',
+		link: 'http://google.com',
+		skip: 2,
+		period: 'Week',
 		amount: 200.00,
 		occurences: [
-			date: Date,
-			complete: true
+			{
+				date: Date,
+				complete: true
+			}
 		]
 	}
 ]
